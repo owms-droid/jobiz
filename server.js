e97a6f8ee9788809ed6589255724032c0eabb4e6
owms-droid <mod20001@byui.edu>
2026-03-28 19:57:25 -0500

    connected mongodb, and setup database

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+mongodb.initDb((err, db) => {
+    if (err) {
+        console.error('Failed to connect to the database');
+    } else {
+        app.listen(port, () => {
+            console.log(`Database is listening and node running on port ${port}`);
+        });
+    }
 });
 
